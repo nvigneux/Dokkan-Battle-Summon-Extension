2de874a07e7da359923f519f73a2fe8d0a1f6702
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-05-25 17:50:38 +0200

    [*] Refacto summons promise

--- public/content.js
+++ public/content.js
@@ -1,23 +1,4 @@
 /**
- * Registers a click event listener on the window object.
- * @param {Function} listener - The callback function to be executed when a click event occurs.
- */
-function registerClickListener(listener) {
-  window.addEventListener('click', listener);
-}
-
-/**
- * Sends a message to the background script indicating that the user has clicked.
- */
-function countClicks() {
-  chrome.runtime.sendMessage({ action: 'USER_CLICKED' }, () => {
-    // console.log('received clicked from background sendResponse', response);
-  });
-}
-
-registerClickListener(countClicks);
-
-/**
  * Utility function to get the rarity of a card.
  * @param {number} rarity - The rarity value of the card.
  * @returns {string} - The rarity of the card.
