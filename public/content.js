7f1ae60a7e93cb253c3bf7ed9746d3c33cc73bb1
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-05-29 18:24:28 +0200

    [*] change structure gasha summon

--- public/content.js
+++ public/content.js
@@ -1,3 +1,8 @@
+const LABEL = {
+  featuredSSRs: 'Featured SSRs',
+  nonFeaturedSSRs: 'Non-Featured SSRs',
+};
+
 /**
  * Utility function to get the rarity of a card.
  * @param {number} rarity - The rarity value of the card.
@@ -67,6 +72,25 @@ const getCardElementSrc = (element) => `../../../../img/layout/cha_type_icon_${e
 const getCardBackground = (element, rarity) => `../../../../img/layout/character_thumb_bg/cha_base_${getCardElement(element)}_${getCardElement(rarity)}.png`;
 
 /**
+ * Creates a section element with the specified id and title.
+ *
+ * @param {string} id - The id of the section element.
+ * @param {string} title - The title of the section element.
+ * @returns {HTMLElement} The created section element.
+ */
+const createSection = (id, title) => {
+  const section = document.createElement('div');
+  section.id = id;
+  section.classList.add('section');
+  section.innerHTML = `
+      <img class="section__dragon-ball" src="../../../img/ui/dragon-ball-loader.png" alt="dragon-ball-loader">
+      <span class="section__title">${title}</span>
+      <img class="section__dragon-ball" src="../../../img/ui/dragon-ball-loader.png" alt="dragon-ball-loader">
+  `;
+  return section;
+};
+
+/**
  * Creates a card element based on the provided card data.
  * @param {Object} card - The card data.
  * @returns {HTMLElement} - The created card element.
@@ -94,12 +118,76 @@ const createCardElement = (card) => {
  * Initializes an element with the specified id if it doesn't already exist.
  * @param {string} id - The id of the element to initialize.
  */
-const initId = (id) => {
+const initId = (id, target) => new Promise((resolve, reject) => {
   if (!document.getElementById(id)) {
     const summonList = document.createElement('div');
     summonList.id = id;
     summonList.classList.add('summon-list');
-    document.body.appendChild(summonList);
+    target.appendChild(summonList);
+    resolve();
+  } else {
+    reject(new Error(`Element with id ${id} already exists.`));
+  }
+});
+
+const initSummonId = (id, target) => new Promise((resolve, reject) => {
+  if (!document.getElementById(id)) {
+    const summonList = document.createElement('div');
+    summonList.id = id;
+    summonList.innerHTML = `
+      <div id="section-${id}"></div>
+      <div id="summon-${id}"></div>
+    `;
+    target.appendChild(summonList);
+    resolve();
+  } else {
+    reject(new Error(`Element with id ${id} already exists.`));
+  }
+});
+
+/**
+ * Initializes a section with the given id and title.
+ * If a section with the given id does not exist,
+ * it creates a new section and appends it to the 'summon-list' element.
+ *
+ * @param {string} id - The id of the section.
+ * @param {string} title - The title of the section.
+ */
+const initSectionId = (id, target, title) => new Promise((resolve, reject) => {
+  if (!document.getElementById(id)) {
+    const section = createSection(id, title);
+    target.appendChild(section);
+    resolve();
+  } else {
+    reject(new Error(`Section with id ${id} already exists.`));
+  }
+});
+
+/**
+ * Finds the animated image element for a given gasha ID.
+ * @param {string} gashaId - The ID of the gasha.
+ * @returns {Element|null} - The animated image element if found, or null if not found.
+ */
+const findAnimatedImgGasha = (gashaId) => {
+  try {
+    const gashaElement = document.getElementById(`gasha_${gashaId}`).parentNode.parentNode.children[2].children[3];
+    return gashaElement;
+  } catch (error) {
+    return null;
+  }
+};
+
+/**
+ * Finds the static image element for a given gasha ID.
+ * @param {string} gashaId - The ID of the gasha.
+ * @returns {HTMLImageElement|null} - The image element if found, or null if not found.
+ */
+const findStaticImgGasha = (gashaId) => {
+  try {
+    const imgElement = document.querySelector(`img[src="../../../img/gashas/gashas_${gashaId}.png"]`);
+    return imgElement.parentNode.parentNode.parentNode.children[2].children[3];
+  } catch (error) {
+    return null;
   }
 };
 
@@ -107,18 +195,27 @@ const initId = (id) => {
  * Initializes the display of the summon list.
  * @returns {Promise<void>} A promise that resolves when all IDs have been successfully initialized.
  */
-const initDisplaySummonList = async () => {
+const initDisplaySummonList = async (gashaId) => {
+  const staticTarget = findStaticImgGasha(gashaId);
+  const animatedTarget = findAnimatedImgGasha(gashaId);
+
+  const target = staticTarget || animatedTarget;
   try {
     await Promise.all([
-      initId('summon-buttons'),
-      initId('summon-result'), // summons results
-      initId('summon-featuredSSRs'),
-      initId('summon-nonFeaturedSSRs'),
-      initId('cards-list'), // init cards list to load all thumbs
+      initSectionId('section-summon', target, 'Summon Simmulator'),
+      initId('summon-stats', target),
+      initId('summon-buttons', target),
+      initId('summon-result', target), // summons results
+
+      initSummonId('featuredSSRs', target),
+      initSummonId('nonFeaturedSSRs', target),
+
+      initId('cards-list', document.body), // init cards list to load all thumbs
     ]);
   } catch (error) {
     console.error('An error occurred during the initialization of IDs:', error);
   }
+  return true;
 };
 
 /**
@@ -176,7 +273,15 @@ const displayCardsList = (id, cards, init = false) => {
 
 const displaySummonedCardsList = (cards, summonsCards, category, init = false) => {
   const cardsList = document.getElementById(`summon-${category}`);
-  if (init) { cardsList.innerHTML = ''; }
+  if (init) {
+    cardsList.innerHTML = '';
+  }
+
+  if (!document.getElementById(`section-${category}`).firstChild) {
+    const section = createSection(`section-${category}`, LABEL[category]);
+    document.getElementById(`section-${category}`).appendChild(section);
+  }
+
   cardsList.classList.add('cards-list');
   cards.forEach((card) => {
     if (!summonsCards[card.id]) {
@@ -200,6 +305,35 @@ const displaySummonedCards = (cards, summonCards) => {
 };
 
 /**
+ * Displays the summon statistics on the page.
+ *
+ * @param {number} totalMulti - The total number of multi summons.
+ * @param {number} totalSingle - The total number of single summons.
+ * @param {number} totalDs - The total number of dragon stones used.
+ */
+const displaySummonStats = (totalMulti, totalSingle, totalDs) => {
+  const summonStats = document.getElementById('summon-stats');
+  summonStats.innerHTML = `
+    <div class="summon-stats__wrapper">
+      <div class="summon-stats__container">
+        <div class="summon-stats__row">
+          <span class="summon-stats__title">Total Multi Summons: </span>
+          <span class="summon-stats__value">${totalMulti}</span>
+        </div>
+        <div class="summon-stats__row">
+          <span class="summon-stats__title">Total Single Summons: </span>
+          <span class="summon-stats__value">${totalSingle}</span>
+        </div>
+      </div>
+      <div class="summon-stats__row">
+        <span class="summon-stats__title">Total Dragon Stones Used: </span>
+        <span class="summon-stats__value">${totalDs}</span>
+      </div>
+    </div>
+  `;
+};
+
+/**
  * Event listener for messages from the background script.
  * @param {Object} message - The message received from the background script.
  */
@@ -212,14 +346,26 @@ chrome.runtime.onMessage.addListener(async (message) => {
       displayCardsList('summon-result', message.result, true);
       break;
     case 'BACKGROUND_UPDATE_STORAGE': {
-      const { summonCards, fetchedUrls } = message.storage;
+      const {
+        fetchedUrls,
+      } = message.storage;
+      const {
+        totalMultiSummons,
+        totalSingleSummons,
+        totalDS,
+        summonCards,
+      } = message.storage[message.gashaId];
+
+      displaySummonStats(totalMultiSummons, totalSingleSummons, totalDS);
       displaySummonedCards(fetchedUrls[message.gashaId], summonCards);
       break;
     }
     case 'REQUEST_INTERCEPTED_GASHA': {
       const { gashaId, data } = message;
-      await initDisplaySummonList();
+
+      await initDisplaySummonList(gashaId);
       displaySummonButtons(gashaId);
+      displaySummonStats(0, 0, 0);
       displayCardsList('cards-list', [...data.featured_cards, ...data.normal_cards], true);
       break;
     }
