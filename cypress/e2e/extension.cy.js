c6666451429f24d05878e9078065b9f33ef70fc7
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2024-07-01 20:13:50 +0200

    [*] revert cypress

--- cypress/e2e/extension.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Ouverture de la page Dokkan Battle Summon', () => {
-  it('should open the specified extension popup', async () => {
-    cy.visit('chrome-extension://mhdjoohjkgnccjfcakmjjaefehknceop/index.html')
-    // essaye avec page goto
-    await page.goto('chrome-extension://mhdjoohjkgnccjfcakmjjaefehknceop/index.html');
-  });
-});
