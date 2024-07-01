bad9dd806b5db5b5c3688e649c16efe4b8bfc7d3
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2024-07-01 20:12:48 +0200

    [*] load cypress extension

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
+const path = require('path');
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents (on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          const extensionPath = path.resolve('./build');
+          launchOptions.args.push(`--load-extension=${extensionPath}`);
+          launchOptions.args.push('--auto-open-devtools-for-tabs');
+        }
+        return launchOptions;
+      });
     },
   },
 });
