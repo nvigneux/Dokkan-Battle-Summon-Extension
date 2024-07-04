f0799687c38ea44c2cabd4b06582a054e1668139
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-07-04 23:31:14 +0200

    [*] test single summon

--- tests/summon.spec.js
+++ tests/summon.spec.js
@@ -13,3 +13,22 @@ test('should check if extension buttons are loaded', async ({ page }) => {
   expect(singleButton).toBeTruthy();
   expect(multiButton).toBeTruthy();
 });
+
+test('should Single Summon on banner', async ({ page }) => {
+  await page.goto('https://jpn.dbz-dokkanbattle.com/summon/1445');
+
+  const buttonConsent = await page.getByLabel('Consent', { exact: true });
+  await buttonConsent.click();
+
+  const singleButton = await page.locator('.summon-button__single').first();
+  await singleButton.click();
+
+  const ds = await page.locator('.summon-stats__ds').first();
+  expect(ds).toContainText('5');
+
+  const resultSummon = await page.locator('#summon-result').first();
+  expect(resultSummon).toBeTruthy();
+
+  const resulsSummonCards = await page.locator('#summon-result .card');
+  expect(resulsSummonCards).toHaveCount(1);
+});
