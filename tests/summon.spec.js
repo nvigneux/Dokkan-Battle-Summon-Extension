870a6b8373bc0c5271987b652059d827738957a4
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-07-04 23:45:16 +0200

    [*] test multi summon

--- tests/summon.spec.js
+++ tests/summon.spec.js
@@ -1,34 +1,64 @@
 // @ts-check
 const { test, expect } = require('./fixtures/load-extension');
 
-test('should check if extension buttons are loaded', async ({ page }) => {
-  await page.goto('https://jpn.dbz-dokkanbattle.com/summon/1445');
-
+const SUMMON_ID = 1445;
+test.beforeEach(async ({ page }) => {
+  await page.goto(`https://jpn.dbz-dokkanbattle.com/summon/${SUMMON_ID}`);
   const buttonConsent = await page.getByLabel('Consent', { exact: true });
   await buttonConsent.click();
+});
 
-  const singleButton = await page.locator('.summon-button__single').first();
-  const multiButton = await page.locator('.summon-button__multi').first();
+/**
+ * Clicks the specified button and checks if it has been successfully clicked.
+ * If the button is not clicked, it recursively calls itself until the button is clicked.
+ * @param {import('playwright/test').ElementHandle} button - The button element to be clicked.
+ * @returns {Promise<void>} - A promise that resolves when the button is successfully clicked.
+ */
+const clickAndCheck = async (button) => {
+  await button.click();
+  // @ts-ignore
+  const clicked = await button.evaluate((el) => (el).classList.contains('summon-button__clicked'));
+  if (!clicked) {
+    await clickAndCheck(button);
+  }
+};
+
+test('should check if extension summon buttons are loaded', async ({ page }) => {
+  const singleButton = await page.getByTestId(`button-single-${SUMMON_ID}`);
+  const multiButton = await page.getByTestId(`button-multi-${SUMMON_ID}`);
 
   expect(singleButton).toBeTruthy();
   expect(multiButton).toBeTruthy();
 });
 
 test('should Single Summon on banner', async ({ page }) => {
-  await page.goto('https://jpn.dbz-dokkanbattle.com/summon/1445');
+  const summonButtons = await page.getByTestId('summon-buttons').locator('button');
+  expect(summonButtons).toHaveCount(2);
 
-  const buttonConsent = await page.getByLabel('Consent', { exact: true });
-  await buttonConsent.click();
+  const summonSingleButton = await page.getByTestId(`button-single-${SUMMON_ID}`);
+  // @ts-ignore
+  await clickAndCheck(summonSingleButton);
+  await page.waitForTimeout(1000); // wait for the summon cards display
 
-  const singleButton = await page.locator('.summon-button__single').first();
-  await singleButton.click();
+  const resultSummon = await page.getByTestId('summon-result').locator('.card');
+  expect(resultSummon).toHaveCount(1);
 
   const ds = await page.locator('.summon-stats__ds').first();
   expect(ds).toContainText('5');
+});
 
-  const resultSummon = await page.locator('#summon-result').first();
-  expect(resultSummon).toBeTruthy();
+test('should Multi Summon on banner', async ({ page }) => {
+  const summonButtons = await page.getByTestId('summon-buttons').locator('button');
+  expect(summonButtons).toHaveCount(2);
 
-  const resulsSummonCards = await page.locator('#summon-result .card');
-  expect(resulsSummonCards).toHaveCount(1);
+  const summonMultiButton = await page.getByTestId(`button-multi-${SUMMON_ID}`);
+  // @ts-ignore
+  await clickAndCheck(summonMultiButton);
+  await page.waitForTimeout(1000); // wait for the summon cards display
+
+  const resultSummon = await page.getByTestId('summon-result').locator('.card');
+  expect(resultSummon).toHaveCount(10);
+
+  const ds = await page.locator('.summon-stats__ds').first();
+  expect(ds).toContainText('50');
 });
