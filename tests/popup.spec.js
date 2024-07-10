3cf55e1b9f135a65811eebd42fd70342bfb02bee
Nicolas Vigneux <nicolas.vigneux@zol.fr>
2024-07-10 23:17:17 +0200

    [*] init test on extension popup

--- tests/popup.spec.js
+++ tests/popup.spec.js
@@ -1,1 +1,9 @@
-// @ts-check
+const { test, expect } = require('./fixtures/loadExtension');
+
+test.beforeEach(async ({ page, extensionId }) => {
+  await page.goto(`chrome-extension://${extensionId}/index.html`);
+});
+
+test('popup page', async ({ page }) => {
+  await expect(page.locator('body')).toHaveText('Dokkan Battle Summons');
+});
